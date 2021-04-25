46e089cc220c7a94eba1ad6f66eafe358a1b9afe
Andrew <verden.kma@gmail.com>
2021-04-26 00:08:58 +0300

    -f fix

--- src/main/java/edu/ukma/rdb/gradesheetparser/ParseService.java
+++ src/main/java/edu/ukma/rdb/gradesheetparser/ParseService.java
@@ -277,8 +277,9 @@ public class ParseService implements IParser {
                 sheet.setIsValid(false);
             }
 
-            if (std.getNationalGrade().equals("Не відвідував") || std.getNationalGrade().equals("Не допущений")
-                    || std.getNationalGrade().equals("Не відвідувала") || std.getNationalGrade().equals("Не допущена")) {
+            if ((std.getNationalGrade().equals("Не відвідував") || std.getNationalGrade().equals("Не відвідувала"))
+                    || ((std.getNationalGrade().equals("Не допущений") || std.getNationalGrade().equals("Не допущена"))
+                    && std.getEctsGrade() == 'F')) {
                 if (std.getSum() == null && std.getExamGrade() == null) {
                     std.setTermGradeError(null);
                     std.setExamGradeError(null);
